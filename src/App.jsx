8fe10d2ae9f084a1bdb493f7bdb1bb6b84d7d471
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-08-08 11:35:37 +0530

    initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-function App() {
+import { Route, Router, Routes } from "react-router";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Events from './pages/Events/Events'
 
+function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/events" element={<Events />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
