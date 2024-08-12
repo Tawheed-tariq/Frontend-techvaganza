4c8e4cbcab1b18c5233fa2a423073ecd2dc3cceb
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-08-12 11:38:49 +0530

    some imp changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,8 @@
-
-import '../../styles/Home.css';
-import Header from'./components/Header';
-
+import Header from'../../Components/Header';
 
 export default function Home(){
     return(
         <> 
-        <div className='container'>
-            <Header />
-        </div>
             <div className="background-image bg-1"></div>
             <div className="background-image bg-2"></div>
             <div className="background-image bg-3"></div>
@@ -16,9 +10,7 @@ export default function Home(){
             <div className="glass-layer"></div>
 
             <div className="content">
-                <div >
-                    <h1>Home</h1>
-                </div>
+                    <Header />
             </div>
         </>
     )
