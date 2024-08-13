66b57dab73f75464113f7d34efd1e8d5cce93423
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-08-13 13:30:02 +0530

    chnages in docker file

--- src/pages/Events/components/EventCard.jsx
+++ src/pages/Events/components/EventCard.jsx
@@ -2,11 +2,11 @@ export default function EventCard({title, time, venue}) {
   return (
     <>
       <div className="rounded-xl flex flex-col items-center h-[500px] shadow-xl shadow-slate-400 w-[310px] px-3 py-2 mb-4">
-        <img
-          className="w-[280px] h-[360px] z-30 absolute -mt-5 rounded-xl shadow-lg transform hover:scale-105 transition duration-200 ease-in-out"
-          src="/events/meme.jpg"
-          alt=""
-        />
+          <img
+            className="w-[280px] h-[360px] z-30 absolute -mt-5 rounded-xl shadow-lg transform hover:scale-105 transition duration-200 ease-in-out"
+            src="/events/meme.jpg"
+            alt=""
+          />
         <div className="mt-auto w-full mb-6 font-playfair">
           <p className="font-semibold text-2xl">
             {title}
