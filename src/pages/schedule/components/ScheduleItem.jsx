ca3a091bd38ecdf05cb421a036e0b8c4127fbafc
Tawheed-tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-08-14 12:37:29 +0530

    some bug fixes

--- src/pages/schedule/components/ScheduleItem.jsx
+++ src/pages/schedule/components/ScheduleItem.jsx
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 
 const ScheduleItem = ({title, startTime, endTime, venue, registerUrl, exploreUrl}) => {
     return (
-      <div class="flex flex-col lg:flex-row lg:justify-between lg:items-center mb-4 px-4 py-4 bg-gray-100">
-        <div class="flex flex-col lg:flex-row lg:items-center">
-          <h3 class="text-xl font-bold">{title}</h3>
-          <span class="mt-2 lg:mt-0 lg:ml-4">{startTime} - {endTime}</span>
+      <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center mb-4 px-4 py-4 bg-gray-100">
+        <div className="flex flex-col lg:flex-row lg:items-center">
+          <h3 className="text-xl font-bold">{title}</h3>
+          <span className="mt-2 lg:mt-0 lg:ml-4">{startTime} - {endTime}</span>
         </div>
-        <div class="mt-2 lg:mt-0">{venue}</div>
-        <div class="mt-2 lg:mt-0 space-x-2">
+        <div className="mt-2 lg:mt-0">{venue}</div>
+        <div className="mt-2 lg:mt-0 space-x-2">
           <Link to={exploreUrl}>
-            <button class="border border-gray-500 font-bold py-2 px-4 rounded">
+            <button className="border border-gray-500 font-bold py-2 px-4 rounded">
               Explore
             </button>
           </Link>
           <Link to={registerUrl}>
-            <button class="border border-gray-500 font-bold py-2 px-4 rounded">
+            <button className="border border-gray-500 font-bold py-2 px-4 rounded">
               Register
             </button>
           </Link>
