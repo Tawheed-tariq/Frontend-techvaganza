e004de1cedfe30508a2818880e2836cbdd3fb94b
Rrrinav <rinavhansa4@gmail.com>
2024-08-14 22:19:32 +0530

    [ Style changes ]: Font change in e-mail

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import { FaInstagram, FaYoutube, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-[url('/common/footer-cover.jpg')] bg-cover bg-top h-96 relative text-white flex flex-col justify-center items-center ">
+    <footer className="bg-[url('/common/footer-cover.jpg')] bg-cover bg-top h-96 relative text-white flex flex-col justify-center items-center font-playfair">
       <div className="absolute inset-0 bg-black opacity-40 z-0"></div>
       <div className="top-0 relative h-auto overflow-hidden w-100%">
         <h2 className="text-[9rem] font-bold tracking-[0.5em] opacity-100 z-2 my-20 mix-blend-overlay text-yellow-200">
@@ -12,8 +12,11 @@ const Footer = () => {
 
       <div className="absolute h-auto left-0 right-0 z-10 flex flex-col justify-around items-center mt-28">
         <div className="container mx-auto text-center">
-          <div className="relative z-10 mb-4 overflow-visible">
-            <a href="#" className="text-8xl font-bold">
+          <div className="relative z-10 mb-4 overflow-visible text-3xl md:text-5xl lg:text-8xl">
+            <a
+              href="#"
+              className="text-2xl sm:text-4xl md:text-6xl lg:text-8xl font-bold"
+            >
               Techvaganza
             </a>
           </div>
@@ -21,7 +24,7 @@ const Footer = () => {
           <div className="mb-4">
             <a
               href="mailto:techvaganza@nitsri.ac.in"
-              className="text-lg hover:underline hover:text-primary"
+              className="text-lg hover:underline hover:text-primary font-averia"
             >
               techvaganza@nitsri.ac.in
             </a>
