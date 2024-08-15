eb21b2aac94786a0e216a6ffd2f0dc1e786e1475
afrah shah <afrahshah1234@gmail.com>
2024-08-15 18:53:07 +0530

    responsive

--- src/Components/PageLayout.jsx
+++ src/Components/PageLayout.jsx
@@ -3,12 +3,12 @@ import Header from './Header'
 export default function PageLayout({ title, imgUrl, children }) {
   return (
     <>
-      <div className="background-image bg-1"></div>
+{/*       <div className="background-image bg-1"></div>
       <div className="background-image bg-2"></div>
       <div className="background-image bg-3"></div>
       <div className="background-image bg-4"></div>
 
-      <div className="glass-layer"></div>
+      <div className="glass-layer"></div> */}
 
       <div className="content">
         <div
