01a2944860dddfb8109ace2c4e4608f0beb722aa
afrah shah <afrahshah1234@gmail.com>
2024-08-16 11:17:37 +0530

    updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,30 +1,26 @@
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 
-import Intro from "./Components/Intro";
-import About from "./Components/About";
+
 import Highlights from "./Components/EventsLineup";
-import Sponsors from "./Components/Sponsors";
-import Events from "./Components/Events";
+
 
 export default function Home() {
   return (
     <>
-      <div className="background-image bg-1"></div>
+{/*       <div className="background-image bg-1"></div>
       <div className="background-image bg-2"></div>
       <div className="background-image bg-3"></div>
 
-      <div className="glass-layer"></div>
+      <div className="glass-layer"></div> */}
 
       <div className="content">
         <Header />
 
         <div className="py-10">
-          <Intro />
-          <About />
+        
           <Highlights />
-          <Sponsors />
-          <Events />
+         
         </div>
         <Footer />
       </div>
