16367c7fb16b445683d63e333f76af2984e78403
“sheen2536” <afsheensajad5@gmail.com>
2024-08-16 20:28:38 +0530

    stickyScroll ..

--- src/pages/Home/Components/stickyScroll.jsx
+++ src/pages/Home/Components/stickyScroll.jsx
@@ -24,7 +24,7 @@ const MediaSidebar = () => {
             href="https://in.linkedin.com/company/techvaganza-nit-srinagar"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center justify-center w-12 h-12 group-hover:w-24 text-white hover:bg-[#d4a76e] transition-all duration-300 ease-in-out"
+            className="flex items-center justify-center w-12 h-12 group-hover:w-24 text-white hover:bg-[#d4a76e] transition-all duration-500 ease-in-out"
           >
             <FaLinkedinIn className="text-lg" />
           </a>
@@ -36,7 +36,7 @@ const MediaSidebar = () => {
             href="https://www.youtube.com/@NITSrinagarofficial"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center justify-center w-12 h-12 group-hover:w-24 text-white hover:bg-[#d4a76e] transition-all duration-300 ease-in-out"
+            className="flex items-center justify-center w-12 h-12 group-hover:w-24 text-white hover:bg-[#d4a76e] transition-all duration-500 ease-in-out"
           >
             <FaYoutube className="text-lg" />
           </a>
